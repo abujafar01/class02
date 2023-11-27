ef58dc11aff007e58bc9ce367260fb6c1c07bd6e
abujafar01 <126306175+abujafar01@users.noreply.github.com>
2023-11-27 22:06:41 +0600

    Add files via upload

--- views/index.php
+++ views/index.php
@@ -1,1 +1,6 @@
-
+<form action="../app/controller/ProductController.php" method="post">
+    <input type="text" name="product_price">
+    <input type="text" name="product_discount">
+    <input type="text" name="product_vat">
+    <input type="submit" name="submit">
+</form>
